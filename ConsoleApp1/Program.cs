29eeae66a20ef3e27fe2ed889a02c5a629a7f5d1
ZhengZicong <578948046@qq.com>
2022-03-18 22:19:58 +0800

    Add csharp7 proj

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+double a = 0.01;
+double b = 0.02;
+Console.WriteLine($"a={a},b={b}, a + b = 0.03? {a + b == 0.03}");
+if (a + b == 0.03)
+{
+    Console.WriteLine(" a + b == 0.03");
+}
+else
+{
+    Console.WriteLine("a + b != 0.03");
+}
+
+double c = 0.1;
+double d = 0.2;
+Console.WriteLine($"c={c},d={d}, c + d = 0.3? {c + d == 0.3}");
+if (a + b == 0.3)
+{
+    Console.WriteLine(" c + d == 0.3");
+}
+else
+{
+    Console.WriteLine(" c + d != 0.3");
+}
+
+double e = 1.0;
+double f = 2.0;
+Console.WriteLine($"e={e},f={f}, e + f = 3? {e + f == 3}");
+if (e + f == 3)
+{
+    Console.WriteLine(" e + f == 3");
+}
+else
+{
+    Console.WriteLine(" e + f != 3");
+}
+    
---
+++ Csharp_v7/Program.cs
@@ -0,0 +1,85 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        // See https://aka.ms/new-console-template for more information
+        //Console.WriteLine("Hello, World!");
+
+
+        (string, string) str = ("text 01", "text 02");
+        Console.WriteLine(string.Format("{0},{1}", str.Item1, str.Item2));
+        Console.WriteLine(str.Item1.GetType()); //str 
+
+        Console.WriteLine("----------------------------------------------");
+
+        (int No, string Name) student = (1, str.Item1);
+        Console.WriteLine($"{student.No}_{student.Name}");
+
+        Console.WriteLine("----------------------------------------------");
+
+        var student2 = (No: "2", Name: "Zhangsan");
+        var student3 = new { No = "3", Name = "Lisi" };
+        Console.WriteLine(string.Format("{0}", student2));
+
+        Console.WriteLine("----------------------------------------------");
+
+        int count = 5;
+        string label = "Colors used in the map";
+        var pair = (count, label);
+        Console.WriteLine(pair);
+
+        Console.WriteLine("----------------------------------------------");
+
+        var fields = student2.GetType().GetFields();
+        foreach (var field in fields)
+            Console.Write($"{field.Name} ");
+        Console.WriteLine("");
+
+        Console.WriteLine("----------------------------------------------");
+
+        var xs = new[] { 4, 7, 9 };
+        var limits = FindMinMax(xs);
+        Console.WriteLine($"Limits of [{string.Join(" ", xs)}] are {limits.min} and {limits.max}");
+
+        Console.WriteLine("----------------------------------------------");
+        Console.WriteLine("-----------------------析构-----------------------");
+
+        var t = ("post office", 3.6);
+        (string destination, double distance) = t;
+        Console.WriteLine($"Distance to {destination} is {distance} kilometers.");
+
+        Console.WriteLine("----------------------------------------------");
+        var (destination1, distance1) = t;
+        Console.WriteLine($"Distance to {destination1} is {distance1} kilometers.");
+
+        Console.WriteLine("----------------------------------------------");
+        var (min, _) = FindMinMax(new[] {1,2,3});
+        Console.WriteLine($"min is {min}");
+
+    }
+
+    static (int min, int max) FindMinMax(int[] input)
+    {
+        if (input is null || input.Length == 0)
+        {
+            throw new ArgumentException("Cannot find minimum and maximum of a null or empty array.");
+        }
+
+        var min = int.MaxValue;
+        var max = int.MinValue;
+        foreach (var i in input)
+        {
+            if (i < min)
+            {
+                min = i;
+            }
+            if (i > max)
+            {
+                max = i;
+            }
+        }
+        return (min, max);
+    }
+
+
+}
